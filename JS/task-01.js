13d24c8a3d37b8d15028e6d10374f054f32c97c7
ErrFi <maryasov.nicolay@gmail.com>
2020-12-15 10:38:24 +0200

    task #2 done

---
+++ JS/task-01.js
@@ -0,0 +1,51 @@
+// ^#1
+// Посчитает и выведет в консоль количество категорий 
+// в ul#categories, то есть элементов li.item.
+// Получится 'В списке 3 категории.'.
+//#2
+// Для каждого элемента li.item в списке ul#categories, 
+// ^найдет и выведет в консоль текст заголовка 
+// элемента (тега h2) и 
+// количество элементов в категории 
+// (всех вложенных в него элементов li).
+
+// Например для первой категории получится:
+
+// Категория: Животные
+// Количество элементов: 4
+console.log("========task 1");
+//===>
+console.log("Task #1. Method 1");
+const parent = document.querySelector("#categories");
+const childList = parent.querySelectorAll("li.item");
+console.log(`В списке ${childList.length} категории.`);
+childList.forEach(item=>{
+    const itemHeader = item.querySelector("h2");
+    console.log("Категория: ", itemHeader.textContent);
+    const elemList = item.querySelectorAll("li");
+    console.log("Количество элементов: ", elemList.length);
+});
+//===>0
+console.log("Task #1. Method 2");
+const categoriesListRef = document.querySelector("#categories");
+const categories = categoriesListRef.children;
+//===>1
+ const items = document.querySelectorAll("#categories li.item");
+ console.log(`В списке ${items.length} категории.`);
+//===>2
+const categoriesArray = [];
+if (categories.length!==0){
+    categoriesArray.push(...categories);
+    categoriesArray.forEach((item)=>{
+        [...item.children].forEach((childItem)=>{
+            if (childItem.localName==="h2"){
+                console.log("Категория: ",childItem.textContent);                
+            };
+            if(childItem.children.length!=0){
+                console.log("Количество элементов: ", childItem.children.length);
+            };            
+        });
+    });
+};
+
+
---
+++ JS/task-02.js
@@ -0,0 +1,30 @@
+console.log("========task 2");
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+// Напиши скрипт, который для каждого элемента массива ingredients
+// создаст отдельный li, после чего вставит все li за одну операцию 
+// в список ul.ingredients. 
+// Для создания DOM-узлов используй document.createElement().
+//============>
+//получаем ссылку на изменяемый узел
+const listRef = document.querySelector("ul#ingredients");
+//console.log(listRef.children);
+// клонируем полученный узел для сохранения существующих праметов и внесения изменений
+const repListRef = listRef.cloneNode();
+// для каждого элемента массива "ingredients" создаём элемент списка
+ingredients.forEach(item=>{
+    const itemRef = document.createElement("li");
+    itemRef.textContent = item;
+    //itemsElementsRef.push(itemRef);
+    repListRef.appendChild(itemRef);
+})
+// console.log(repListRef);
+listRef.replaceWith(repListRef);
+const newListRef = document.querySelector("ul#ingredients");
+// console.log(newListRef);
